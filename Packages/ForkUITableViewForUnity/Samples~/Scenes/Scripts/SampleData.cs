3a27a3a722b558985d9ddd350f3e258c4ee8ba7a
卢旭鸣 <luxuming@xmfunny.com>
2024-02-02 20:01:31 +0800

    <fix> UITableViewForUnity

---
+++ Packages/ForkUITableViewForUnity/Samples~/Scenes/Scripts/SampleData.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+public class SampleData
+{
+	public enum SampleType
+	{
+		Text,
+		Image,
+		Tab,
+		Chat,
+	}
+
+	public SampleType sampleType;
+	public string text;
+	public int rarity;
+	public bool isFavorite;
+	public float scalar;
+	public int spriteIndex;
+
+	// expend
+	public float scalarBeforeExpand;
+	public float scalarAfterExpand;
+	public bool isExpanded = false;
+	public Action<int> onExpand;
+}
